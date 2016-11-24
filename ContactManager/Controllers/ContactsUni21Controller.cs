9835ad90a045752e7a50f0f18faf30a0a2c6d9bb
Angelo Pitarresi <148693@student.ue.wroc.pl>
2016-11-24 13:54:21 +0100

    v4.0

--- ContactManager/Controllers/ContactsUni21Controller.cs
+++ ContactManager/Controllers/ContactsUni21Controller.cs
@@ -23,6 +23,10 @@ namespace ContactManager.Controllers
             CU.Contattis = db2.Contatti.ToList();
             CU.Contacts = db.Contacts.ToList();
             CU.Companies = db.Companies.ToList();
+            foreach (var contact in CU.Contacts)
+            {
+                contact.Companies = CU.Companies.Where(com => com.CompanyId == contact.CompanyId).ToList();
+            }
             List<ContactsUni2> contactlist = new List<ContactsUni2>();
             contactlist.Add(CU);
             return View(contactlist);
